dbef90df052afdc88ea20465362401b98540daa2
Vladislav <nicochan00@mail.ru>
2022-07-11 02:41:16 +0300

    create button slider

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const cards = Array.from(document.querySelectorAll('.gallery__card'));
 const pictire = Array.from(document.querySelectorAll('.gallery__card_pic'));
 const slider = document.querySelector('.slider');
 const sliderContainer = document.querySelector('.slider__container');
+const sliderButtonLeft = document.querySelector('.slider__btn_left');
+const sliderButtonRight = document.querySelector('.slider__btn_right');
+const sliderButtonClose = document.querySelector('.slider__close');
 
 let cardIndex = 0;
 let pictureFull;
