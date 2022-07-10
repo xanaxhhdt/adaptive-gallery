f4b7f33ffbbea4283f3b04317275140b9feb01c2
Vladislav <nicochan00@mail.ru>
2022-07-11 02:28:08 +0300

    display photo in slider

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 'use strict';
 
-const cards = document.querySelectorAll('.gallery__card');
+const cards = Array.from(document.querySelectorAll('.gallery__card'));
+const pictire = Array.from(document.querySelectorAll('.gallery__card_pic'));
 const slider = document.querySelector('.slider');
+const sliderContainer = document.querySelector('.slider__container');
+
+let cardIndex = 0;
+let pictureFull;
 
 cards.forEach(item => {
    item.addEventListener('click', (event) => {
+      cardIndex = cards.indexOf(item);
+
+      pictureFull = pictire[cardIndex];
+      pictureFull.style.objectFit = 'contain';
+      sliderContainer.append(pictureFull);
+
       slider.classList.add('active');
    });
 });
