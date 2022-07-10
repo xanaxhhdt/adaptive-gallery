92b4a8c74c7c0824365360c6897494e42a9f9d77
Vladislav <nicochan00@mail.ru>
2022-07-11 02:15:52 +0300

    slider display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const cards = document.querySelectorAll('.gallery__card');
+const slider = document.querySelector('.slider');
+
+cards.forEach(item => {
+   item.addEventListener('click', (event) => {
+      slider.classList.add('active');
+   });
+});
