044beff4a30da51b04c36dd8759ad0621d4de331
Vladislav <nicochan00@mail.ru>
2022-07-11 03:04:34 +0300

    listerner button(ending)

--- js/script.js
+++ js/script.js
@@ -13,12 +13,51 @@ let pictureFull;
 
 cards.forEach(item => {
    item.addEventListener('click', (event) => {
+      event.preventDefault();
+
       cardIndex = cards.indexOf(item);
 
-      pictureFull = pictire[cardIndex];
+      pictureFull = pictire[cardIndex].cloneNode();
       pictureFull.style.objectFit = 'contain';
       sliderContainer.append(pictureFull);
 
       slider.classList.add('active');
    });
+});
+
+sliderButtonLeft.addEventListener('click', (event) => {
+   event.preventDefault();
+   changePicture('left');
+});
+
+sliderButtonRight.addEventListener('click', (event) => {
+   event.preventDefault();
+   changePicture('right');
+});
+
+function changePicture(element) {
+   if (element === 'left') {
+      if (cardIndex > 0) {
+         cardIndex--;
+      } else {
+         cardIndex = cards.length - 1;
+      }
+   } else if (element === 'right') {
+      if (cardIndex < cards.length - 1) {
+         cardIndex++;
+      } else {
+         cardIndex = 0;
+      }
+   }
+   let newPictureFull = pictire[cardIndex].cloneNode();
+   newPictureFull.style.objectFit = 'contain';
+   pictureFull.replaceWith(newPictureFull);
+   pictureFull = newPictureFull;
+}
+
+sliderButtonClose.addEventListener('click', (event) => {
+   event.preventDefault();
+
+   slider.classList.remove('active');
+   pictureFull.remove();
 });
